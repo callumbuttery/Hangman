fb732e507d039829e6f22a764f6e0c17a82b5f91
Callum Buttery <callumbuttery@icloud.com>
2023-02-01 14:02:35 +0000

    Feat: add hangman frame

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,7 +1,14 @@
 export const HangmanDrawing: React.FC = () => {
     return (
-        <div>
+        <div style={{ position:  "relative"}}>
             
+            <div style={{ height: "50px", width: "10px", top: 0, right: 0, position: 'absolute', background: 'black', marginLeft: "120px"}}></div>
+
+            <div style={{ height: "10px", width: "200px", background: 'black', marginLeft: "120px"}}></div>
+
+            <div style={{ height: "400px", width: "10px", background: 'black', marginLeft: "120px"}}></div>
+
+            <div style={{ height: "10px", width: "250px", background: 'black'}}></div>
         </div>
     )
 }
